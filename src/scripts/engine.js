ca2801085ecef8ab8d492e2d9c30c8a6c502d3ff
marques <gustavosmarques19@gmail.com>
2024-12-06 21:26:13 -0300

    projeto finalizado

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -2,11 +2,11 @@ const pianoKeys = document.querySelectorAll(".piano-keys .key");
 const volumeSlider = document.querySelector(".volume-slider input");
 const keysCheck = document.querySelector(".keys-check input");
 
-let mapedKeys = [];
-let audio = new Audio("src/tunes/a.wav");
+let mappedKeys = [];
+let audio = new Audio("./src/tunes/a.wav");
 
 const playTune = (key) => {
-  audio.src = `src/tunes/${key}.wav`;
+  audio.src = `./src/tunes/${key}.wav`;
   audio.play();
 
   const clickedKey = document.querySelector(`[data-key="${key}"]`);
@@ -18,11 +18,11 @@ const playTune = (key) => {
 
 pianoKeys.forEach((key) => {
   key.addEventListener("click", () => playTune(key.dataset.key));
-  mapedKeys.push(key.dataset.key);
+  mappedKeys.push(key.dataset.key);
 });
 
 document.addEventListener("keydown", (e) => {
-  if (mapedKeys.includes(e.key)) {
+  if (mappedKeys.includes(e.key)) {
     playTune(e.key);
   }
 });
@@ -31,10 +31,10 @@ const handleVolume = (e) => {
   audio.volume = e.target.value;
 };
 
+volumeSlider.addEventListener("input", handleVolume);
+
 const showHideKeys = () => {
   pianoKeys.forEach((key) => key.classList.toggle("hide"));
 };
-
-volumeSlider.addEventListener("input", handleVolume);
 
 keysCheck.addEventListener("click", showHideKeys);
